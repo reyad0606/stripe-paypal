1b73cf74604682ccdd97319402b3ac0f2817916f
RM <70502707+reyad0606@users.noreply.github.com>
2021-03-22 16:18:05 -0400

    Seconf Commit

--- src/main/java/com/example/demo/PaypalController.java
+++ src/main/java/com/example/demo/PaypalController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.paypal.api.payments.Links;
@@ -20,7 +21,7 @@ public class PaypalController {
 	public static final String SUCCESS_URL = "pay/success";
 	public static final String CANCEL_URL = "pay/cancel";
 
-	@GetMapping("/")
+	@RequestMapping("/")
 	public String home() {
 		return "home";
 	}
