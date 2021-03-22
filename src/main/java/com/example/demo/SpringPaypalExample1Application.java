0b9d7ca3a2d3ff063d27e1b657e29a9d6fb86a46
RM <70502707+reyad0606@users.noreply.github.com>
2021-03-22 15:48:59 -0400

    First Commit

---
+++ src/main/java/com/example/demo/SpringPaypalExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaypalExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaypalExample1Application.class, args);
+	}
+
+}
